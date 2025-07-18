1e687ddea6e1f88f94f5f8022cb8d18cd083c580
a-l-g <agjgardner@virginmedia.com>
2025-07-18 14:41:33 +0100

    Update gt.js

--- gt.js
+++ gt.js
@@ -1,4 +1,4 @@
-	const map = L.map('map').setView([51.505, -0.09], 13);
+	const map = L.map('map').setView([51.505, -0.09], 8);
 
 	const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		maxZoom: 19,
